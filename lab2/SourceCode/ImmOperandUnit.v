01e6df4a6af03854905e46295c99d105c1693c4b
alicemare <1479070232@qq.com>
2021-04-17 16:59:53 +0800

    ImmPerandUnit finished

--- lab2/SourceCode/ImmOperandUnit.v
+++ lab2/SourceCode/ImmOperandUnit.v
@@ -38,7 +38,11 @@ module ImmOperandUnit(
     begin
         case(Type)
             `ITYPE: Out<={ {21{In[31]}}, In[30:20] };
-            //......                                        //请完善代码
+            `RTYPE: Out <= 32'b0;
+            `STYPE: Out <= { { 21{ In[31] } }, In[30:25], In[11:7] };
+            `BTYPE: Out <= { { 20{ In[31] } }, In[7], In[30:25], In[11:8], 1'b0 };
+            `UTYPE: Out <= { In[31:12], 12'b0};
+            `JTYPE: Out <= { { 12{ In[31] } }, In[19:12], In[20], In[30:25], In[24:21], 1'b0 };
             default:Out<=32'hxxxxxxxx;
         endcase
     end
